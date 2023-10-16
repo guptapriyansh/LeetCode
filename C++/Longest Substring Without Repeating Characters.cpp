5a941206104ec85491b8b790eac67346749cee55
Priyansh Gupta <63574195+guptapriyansh@users.noreply.github.com>
2023-10-16 23:21:12 +0530

    Create Longest Substring Without Repeating Characters.cpp

--- C++/Longest Substring Without Repeating Characters.cpp
+++ C++/Longest Substring Without Repeating Characters.cpp
@@ -4,3 +4,16 @@ Date: Oct 16, 2023
 Problem URL: https://leetcode.com/problems/longest-substring-without-repeating-characters/
 */
 
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int maxLength = 0, temp = 0;
+        vector<int> charIndex(128, -1);
+        for (int index = 0; index < s.length(); index++) {
+            temp = charIndex[s[index]] >= temp ? charIndex[s[index]] + 1 : temp;
+            charIndex[s[index]] = index;
+            maxLength = max(maxLength, index - temp + 1);
+        }
+        return maxLength;
+    }
+};
